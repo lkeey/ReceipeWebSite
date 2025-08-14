17dafd917ed34d71c2fd6bc4287a669575a1f823
Aleksey Kiryushin <97094952+lkeey@users.noreply.github.com>
2025-08-14 12:52:35 +0300

    create databases

--- test.py
+++ test.py
@@ -1,14 +1,25 @@
 import json
 
-user = {
-    "username": "aleksey",
-    "email": "l.key14@mail.ru",
-    "password": "12345678",
+"""
+Пользователи
+"""
+
+users = {
+    "aleksey": {
+        "username": "aleksey",
+        "email": "l.key14@mail.ru",
+        "password": "12345678",
+    },
+    "maksim": {
+        "username": "maksim",
+        "email": "maksim@mail.ru",
+        "password": "ahwduahwidaiwd",
+    },
 }
 
 # для добавления данных
 with open("database/users.json", "w") as f:
-    json.dump(user, f)
+    json.dump(users, f)
 
 # для чтения
 with open("database/users.json", "r") as f:
@@ -16,13 +27,36 @@ with open("database/users.json", "r") as f:
     print("users read:", users_read)
 
 
-# receipe = {
-#     "name": "",
-#     "description": "",
-#     "image_url": "",
-#     "category": "",
-#     "time": 30,
-#     "portion": 5,
-#     "type": "",
-#     "ingredients": ["мука", "масло"],
-# }
+"""
+Рецепты
+"""
+
+receipes = {
+    "лазания": {
+        "name": "лазания",
+        "description": "крутой рецепт",
+        "image_url": "url",
+        "category": "лапша",
+        "time": 30,
+        "portion": 5,
+        "type": "средне",
+        "ingredients": ["мука", "масло"],
+    },
+    "борщ": {
+        "name": "борщ",
+        "description": "крутой борщ",
+        "image_url": "url",
+        "category": "суп",
+        "time": 120,
+        "portion": 10,
+        "type": "легко",
+        "ingredients": ["картофель", "капуста"],
+    },
+}
+
+with open("database/receipes.json", "w") as file:
+    json.dump(receipes, file)
+
+with open("database/receipes.json", "r") as file:
+    receipe_read = json.load(file)
+    print("receipe read:", receipe_read)
