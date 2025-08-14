354b5e7ba2f2802bbc873d1fbb1943576166114c
Aleksey Kiryushin <97094952+lkeey@users.noreply.github.com>
2025-08-14 13:21:40 +0300

    add ability to register

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
+import json
 
 app = Flask(__name__)
 
@@ -13,10 +14,35 @@ def login():
     return render_template("login.html")
 
 
-@app.route("/register")
+@app.route("/register", methods=["POST", "GET"])
 def register():
+    if request.method == "POST":
+        username = request.form["username"]
+        email = request.form["email"]
+        password = request.form["password"]
+        confirmed_password = request.form["confirmed_password"]
+
+        if password != confirmed_password:
+            return "пароли не совпадают"
+        if len(username) < 3:
+            return "имя короткое"
+
+        with open("database/users.json", "r") as f:
+            users = json.load(f)
+
+        users[username] = {
+            "username": username,
+            "email": email,
+            "password": password,
+        }
+
+        with open("database/users.json", "w") as f:
+            json.dump(users, f)
+
+        return redirect("/")
+
     return render_template("register.html")
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, port=8080)
